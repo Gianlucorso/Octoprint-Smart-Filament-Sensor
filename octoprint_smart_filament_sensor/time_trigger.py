ee4baa0f1b982862b3214a55c220168382825e57
Gianluca Corsini <gianluca.corsini@laas.fr>
2020-12-27 18:08:50 +0100

    [master] Add arguments when calling methods inside TimeTrigger class

--- octoprint_smart_filament_sensor/time_trigger.py
+++ octoprint_smart_filament_sensor/time_trigger.py
@@ -32,7 +32,7 @@ class TimeTrigger(THREADING.Thread):
         if (not self._started): #first time run method is called is by start method
             self._logger.debug("Time Trigger: starting")
             self._started = True #thread has been started at least once (from official doc: if started more than once, then runtime error)
-            self._set() #set trigger
+            self._set(self) #set trigger
 
         while self._started:
 
@@ -40,12 +40,13 @@ class TimeTrigger(THREADING.Thread):
                 elapsed_time = (TIME.time() - self._start_time)
 
                 if (elapsed_time > self.time_threshold):
-                    self.fire()
+                    self.fire(self)
 
             TIME.sleep(0.250)
         #GPIO.remove_event_detect(self.used_pin)
 
     def _set(self):
+        self._logger.debug("Time Trigger: running is " + str(self._running))
         if (not self._running):
             self._running = True
             GPIO.remove_event_detect(self._pin) #remove any previous event on the monitored pin
@@ -60,7 +61,7 @@ class TimeTrigger(THREADING.Thread):
         if(self.callback != None):
             self.callback()
         self._logger.debug("Time Trigger: fire done");
-        self.release()
+        self.release(self)
 
     def release(self):
         self._running = False
@@ -69,8 +70,8 @@ class TimeTrigger(THREADING.Thread):
 
     def reset(self):
         self._running = False #thus set will work
-        self._set() #set again trigger
-        self._reset_time(self._pin) #reset internal time
+        self._set(self) #set again trigger
+        self._reset_time(self, self._pin) #reset internal time
         self._logger.debug("Time Trigger: reset done")
 
     def isRunning(self):
